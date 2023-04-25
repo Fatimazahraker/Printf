013b1a0d7f92259e3718eded348644af693a7ffb
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-04-25 13:56:07 -0700

    update

--- print_functions.c
+++ print_functions.c
@@ -58,17 +58,17 @@ int print_string(va_list ap, params_t *params)
 			j = pad = params->precision;
 		if (params->minus_flag)
 		{
-			if (params->precision != UNIT_MAX)
+			if (params->precision != UINT_MAX)
 				for (i = 0; i < pad; i++)
 					sum += _putchar(*str++);
-			else 
+			else
 				sum += _puts(str);
 		}
 		while (j++ < params->width)
 			sum += _putchar(pad_char);
 		if (!params->minus_flag)
 		{
-			if (params->precision !=UNIT_MAX)
+			if (params->precision != UINT_MAX)
 				for (i = 0 ; i < pad ; i++)
 					sum += _putchar(*str++);
 			else
