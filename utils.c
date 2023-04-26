6f3cf7a2837e93a35749d63e608343b3eb75cd0f
Hajar32 <moustaouihajar06@gmail.com>
2023-04-26 09:59:44 -0700

    printf

--- utils.c
+++ utils.c
@@ -34,7 +34,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	bufffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 
-	return(3);
+	return (3);
 }
 
 /**
@@ -43,4 +43,44 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
 
+	return (0);
+}
+
+/**
+ * convert_size_number - casts a number to the specified size
+ * @num: number to be casted
+ * @size: number indicating the type to be casted
+ *
+ * Return: casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - casts a number to the specified size
+ * @num: number to be casted
+ * @size: number indicating the type to be casted
+ *
+ * Return: casted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
