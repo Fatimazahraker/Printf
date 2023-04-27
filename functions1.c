fb47483dc186bc94f41ea3c27bbb9da8632cf2b1
Hajar32 <moustaouihajar06@gmail.com>
2023-04-26 17:12:58 -0700

    printf

--- functions1.c
+++ functions1.c
@@ -39,7 +39,7 @@ int print_unsigned(va_list types, char buffer[],
  * @types: lista of arguments
  * @buffer: buffer array to handle print
  * @flags: calculates active flags
- * @@width: get width
+ * @width: get width
  * @precision: precision specification
  * @size: size specifier
  * Return: number of chars printed
@@ -85,4 +85,72 @@ int print_octal(va_list types, char buffer[],
  * @size: size specifier
  * Return: number of chars printed
  */
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789abcdef", buffer,
+				flags, 'x', width, precision, size));
+}
 
+/**
+ * print_hexa_upper - prints an unsigned number in upper hexadecimal notation
+ * @types: lista of arguments
+ * @buffer: buffer array to handle print
+ * @flags: calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size specifier
+ * Return: number of chars printed
+ */
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789ABCDEF", buffer,
+		flags, 'X', width, precision, size));
+}
+
+/**
+ * print_hexa - prints a hexadecimal number in lower or upper
+ * @types: lista of arguments
+ * @map_to: array of values to map the number to
+ * @buffer: buffer array to handle print
+ * @flags: calculates active flags
+ * @flag_ch: calculates active flags
+ * @width: get width
+ * @precision: precision specification
+ * @size: size speciifier
+ * @size: size specification
+ * Return: number of chars printed
+ */
+int print_hexa(va_list types, char map_to[], char buffer[],
+	int flags, char flag_ch, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[i--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[i--] = map_to[num % 16];
+		num /= 16;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+	{
+		buffer[i--] = flag_ch;
+		buffer[i--] = '0';
+	}
+
+	i++;
+
+	return (write_ynsgnd(0, i, buffer, flags, width, precision, size));
+}
